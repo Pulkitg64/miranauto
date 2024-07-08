6afb6ef1660350fd5c9433e721f6bfef9b7c2bec
Pulkitg64 <pulkitgupta64@gmail.com>
2024-07-08 10:46:56 +0530

    Added logo on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 
 const Navbar = () => {
@@ -14,6 +15,7 @@ const Navbar = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
+          <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
             <span className="text-xl tracking-tight">Miraan Auto</span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
